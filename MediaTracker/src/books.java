b7c9bbc1416e94386ab1cb048d60dbb06034b976
SNY-P <38732482+SNY-P@users.noreply.github.com>
2024-05-23 15:27:23 +0100

    Split the select for translated/untranslated novels + not finished + needs testing

--- MediaTracker/src/books.java
+++ MediaTracker/src/books.java
@@ -2,48 +2,100 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class books {
     dbConnection dbc = new dbConnection();
     // view the table
     public void booksSelect(int uID) throws SQLException {
-        String query = "SELECT b.booksID, b.Name, b.Status, b.Format, b.Chapters, c.Name as Country " +
-                       "FROM Books b " + 
-                       "LEFT JOIN Country c ON b.Country = c.countryID " +
-                       "WHERE userID = ?";
-        
-        try (Connection conn = dbConnection.connect();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
-             
-            pstmt.setInt(1, uID);
-            try (ResultSet rst = pstmt.executeQuery()) {
-                int[] lengths = bEntryLength(uID, conn);
-                System.out.printf("%-" + lengths[0] + "s%-" + lengths[1] + "s%-" + lengths[2] + "s%-" + lengths[3] + "s%-" + lengths[4] + "s%-" + lengths[5] + "s\n",
-                                  "Books ID", "Name", "Status", "Format", "Chapters Read", "Country");
+        Scanner scanner = new Scanner(System.in);
 
-                while (rst.next()) {
-                    int id = rst.getInt("booksID");
-                    String name = rst.getString("Name");
-                    String status = rst.getString("Status");
-                    String format = rst.getString("Format");
-                    int chaptersRead = rst.getInt("Chapters");
-                    String country = rst.getString("Country");
-                    System.out.printf("%-" + lengths[0] + "d%-" + lengths[1] + "s%-" + lengths[2] + "s%-" + lengths[3] + "s%-" + lengths[4] + "d%-" + lengths[5] + "s\n",
-                                      id, name, status, format, chaptersRead, country);
+        while(true) {
+            System.out.println("1. Non-Translated Novels | 2. Translated Novels");
+            System.out.print("Choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                // select db for non translated novels
+                // To Select: id, name, status, format, country, source, chapters
+    
+                String query = "SELECT b.booksID, b.Name, b.Status, b.Format, b.Source, b.Chapters, c.Name as Country " + 
+                               "FROM Books b LEFT JOIN Country c ON b.countryID = c.countryID WHERE userID = ?";
+    
+                try (Connection conn = dbConnection.connect();
+                     PreparedStatement ps = conn.prepareStatement(query)) {
+                    ps.setInt(1, uID);
+    
+                    try (ResultSet rs = ps.executeQuery()) {
+                        int[] lengths = bEntryLength(uID, conn);
+                        System.out.printf("%-" + lengths[0] + "s%-" + lengths[1] + "s%-" + lengths[2] + "s%-" + lengths[3] + "s%-" + lengths[7] + "s%-" + lengths[5] + "s\n",
+                                  "Books ID", "Name", "Status", "Format", "Country", "Source", "Chapters Read");
+    
+                        while (rs.next()) {
+                            int id = rs.getInt("booksID");
+                            String name = rs.getString("Name");
+                            String status = rs.getString("Status");
+                            String format = rs.getString("Format");
+                            int chaptersRead = rs.getInt("Chapters");
+                            String country = rs.getString("Country");
+                            String source = rs.getString("Source");
+                            System.out.printf("%-" + lengths[0] + "s%-" + lengths[1] + "s%-" + lengths[2] + "s%-" + lengths[3] + "s%-" + lengths[7] + "s%-" + lengths[5] + "s\n",
+                                              id, name, status, format, country, source, chaptersRead);
+                        }
+                    } 
                 }
+                catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+                scanner.close();
+                return;
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            else {
+                // select db for translated novels
+                // To Select: id, name, status, format, country, originalname, source, chapters
+                String query = "SELECT b.booksID, b.Name, b.Status, b.Format, b.OriginalName, b.Source, b.Chapters, c.Name as Country " + 
+                               "FROM Books b LEFT JOIN Country c ON b.countryID = c.countryID WHERE userID = ?";
+    
+                try (Connection conn = dbConnection.connect();
+                PreparedStatement ps = conn.prepareStatement(query)) {
+                ps.setInt(1, uID);
+    
+                    try (ResultSet rs = ps.executeQuery()) {
+                        int[] lengths = bEntryLength(uID, conn);
+                        System.out.printf("%-" + lengths[0] + "s%-" + lengths[1] + "s%-" + lengths[2] + "s%-" + lengths[3] + "s%-" + lengths[7] + "s%-" + lengths[4] + "s%-" + lengths[5] + "s\n",
+                                  "Books ID", "Name", "Status", "Format", "Country", "COO Name", "Source", "Chapters Read");
+    
+                        while (rs.next()) {
+                            int id = rs.getInt("booksID");
+                            String name = rs.getString("Name");
+                            String status = rs.getString("Status");
+                            String format = rs.getString("Format");
+                            String cooName = rs.getString("OriginalName");
+                            int chaptersRead = rs.getInt("Chapters");
+                            String country = rs.getString("Country");
+                            String source = rs.getString("Source");
+                            System.out.printf("%-" + lengths[0] + "s%-" + lengths[1] + "s%-" + lengths[2] + "s%-" + lengths[3] + "s%-" + lengths[7] + "s%-" +  lengths[4] + "s%-" + lengths[5] + "s\n",
+                                              id, name, status, format, country, cooName, source, chaptersRead);
+                        }
+                    } 
+                }
+                catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+                scanner.close();
+                return;
+            }
         }
     }
     // used to evenly space out the table
     private int[] bEntryLength(int uID, Connection conn) throws SQLException {
-        String query = "SELECT b.booksID, b.Name, b.Status, b.Format, b.Chapters, c.Name as Country " +
+        String query = "SELECT b.booksID, b.Name, b.Status, b.Format, b.OriginalName, b.Source, b.Chapters, c.Name as Country " +
                        "FROM Books b " + 
-                       "LEFT JOIN Country c ON b.Country = c.countryID " +
+                       "LEFT JOIN Country c ON b.countryID = c.countryID " +
                        "WHERE userID = ?";
 
-        int[] columnLengths = new int[6];
+        int[] columnLengths = new int[8];
 
         try (PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setInt(1, uID);
@@ -52,6 +104,8 @@ public class books {
                 int maxNameLength = 0;
                 int maxStatusLength = 0;
                 int maxFormatLength = 0;
+                int maxOriginalNameLength = 0;
+                int maxSourceLength = 0;
                 int maxChaptersReadLength = 0;
                 int maxCountryLength = 0;
 
@@ -60,6 +114,8 @@ public class books {
                     maxNameLength = Math.max(maxNameLength, rst.getString("Name").length());
                     maxStatusLength = Math.max(maxStatusLength, rst.getString("Status").length());
                     maxFormatLength = Math.max(maxFormatLength, rst.getString("Format").length());
+                    maxOriginalNameLength = Math.max(maxOriginalNameLength, rst.getString("OriginalName").length());
+                    maxSourceLength = Math.max(maxSourceLength, rst.getString("Source").length());
                     maxChaptersReadLength = Math.max(maxChaptersReadLength, String.valueOf(rst.getInt("Chapters")).length());
                     maxCountryLength = Math.max(maxCountryLength, String.valueOf(rst.getInt("Country")).length());
                 }
@@ -68,8 +124,10 @@ public class books {
                 columnLengths[1] = maxNameLength + 10;
                 columnLengths[2] = maxStatusLength + 10;
                 columnLengths[3] = maxFormatLength + 10;
-                columnLengths[4] = maxChaptersReadLength + 10;
-                columnLengths[5] = maxCountryLength + 10;
+                columnLengths[4] = maxOriginalNameLength + 10;
+                columnLengths[5] = maxSourceLength + 10;
+                columnLengths[6] = maxChaptersReadLength + 10;
+                columnLengths[7] = maxCountryLength + 10;
             }
         } catch (SQLException e) {
             e.printStackTrace();
